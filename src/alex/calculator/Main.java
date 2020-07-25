989b800339e7e4fdc27ba3baa570b53522630094
AlexDemiurg <68776043+AlexDemiurg@users.noreply.github.com>
2020-07-25 17:19:35 +0500

    Update Main.java

--- src/alex/calculator/Main.java
+++ src/alex/calculator/Main.java
@@ -1,8 +1,5 @@
 package alex.calculator;
-//AlexDemiurg
-//AlexDemiurg007@yandex.ru
-//cjkytxysqpfqxbr6-ya
-//cjkytXYsqpFQXbr7-git
+
 public class Main {
 
     public static void main(String[] args) {
